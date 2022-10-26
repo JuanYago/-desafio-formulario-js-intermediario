54d873475d6d541e65216bcd25080e06a555a847
Juan Yago <juanyago25@gmail.com>
2022-10-26 20:05:03 -0300

    Primeiro commit

---
+++ src/script.js
@@ -0,0 +1,26 @@
+
+const button = document.getElementById('button')
+const inputs = document.querySelectorAll('.campo')
+const msgErro = document.querySelectorAll('.erro')
+
+button.addEventListener('click', (event) => {
+    event.preventDefault()
+
+
+    console.log(inputs)
+    
+    inputs.forEach((input, indice) => {
+
+        
+        if (input.value === '') {
+            input.classList.add("borda-vermelha")
+            msgErro[indice].classList.add('mostrar')
+
+        } else {
+            input.classList.remove('borda-vermelha')
+            input.classList.add('borda-verde')
+            msgErro[indice].classList.remove('mostrar')
+        }
+    
+    })
+})
